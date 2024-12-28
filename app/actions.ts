022389184e9a6d659b3d9aeaab53afecc85ddfa0
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2024-12-29 00:01:23 +0700

    build availability action is complete

--- app/actions.ts
+++ app/actions.ts
@@ -5,6 +5,7 @@ import { requireUser } from './lib/hooks';
 import { parseWithZod } from '@conform-to/zod';
 import { onboardingSchemaValidation, settingSchema } from './lib/zodSchemas';
 import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
 
 export async function OnboardingAction(prevState: any, formData: FormData) {
   const session = await requireUser();
@@ -104,4 +105,48 @@ export async function SettingAction(prevState: any, formData: FormData) {
   });
 
   return redirect('/dashboard');
+}
+
+export async function updateAvailabilityAction(formData: FormData) {
+  const session = await requireUser();
+
+  const rawData = Object.fromEntries(formData.entries());
+  // console.log('rawData is', rawData);
+
+  const availabilityData = Object.keys(rawData)
+    .filter((key) => key.startsWith('id-'))
+    .map((key) => {
+      const id = key.replace('id-', '');
+
+      return {
+        id,
+        isActive: rawData[`isActive-${id}`] === 'on',
+        fromTime: rawData[`fromTime-${id}`] as string,
+        tillTime: rawData[`tillTime-${id}`] as string,
+      };
+    });
+
+  /* prisma.$transaction:
+  Gửi một loạt truy vấn tới cơ sở dữ liệu trong một giao dịch.
+  Nếu bất kỳ truy vấn nào thất bại, toàn bộ giao dịch sẽ bị hủy. */
+  try {
+    await prisma.$transaction(
+      availabilityData.map((item) =>
+        prisma.availability.update({
+          where: {
+            id: item.id,
+          },
+          data: {
+            isActive: item.isActive,
+            fromTime: item.fromTime,
+            tillTime: item.tillTime,
+          },
+        }),
+      ),
+    );
+
+    revalidatePath('/dashboard/availability');
+  } catch (error) {
+    console.log(error);
+  }
 }
--- app/dashboard/availability/page.tsx
+++ app/dashboard/availability/page.tsx
@@ -15,12 +15,15 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
+  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
 import { notFound } from 'next/navigation';
 import { times } from '@/app/lib/times';
 import { SubmitButton } from '@/app/components/SubmitButton';
+import { Input } from '@/components/ui/input';
+import { updateAvailabilityAction } from '@/app/actions';
 
 async function getData(userId: string) {
   const data = await prisma.availability.findMany({
@@ -48,49 +51,57 @@ export default async function AvailabilityRoute() {
           Ở đây bạn có thể quản lý lịch trình của mình trong tuần
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex flex-col gap-y-4">
-        {data.map((item) => (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center gap-4">
-            <div key={item.id} className="flex items-center gap-x-3">
-              <Switch defaultChecked={item.isActive} />
-              <p>{displayFormatDays(item.day as Day)}</p>
+      <form action={updateAvailabilityAction}>
+        <CardContent className="flex flex-col gap-y-4">
+          {data.map((item) => (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center gap-4">
+              <div key={item.id} className="flex items-center gap-x-3">
+                <Input type="hidden" name={`id-${item.id}`} value={item.id} />
+                <Switch
+                  name={`isActive-${item.id}`}
+                  defaultChecked={item.isActive}
+                />
+                <p>{displayFormatDays(item.day as Day)}</p>
+              </div>
+
+              <Select name={`fromTime-${item.id}`} defaultValue={item.fromTime}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Thời gian bắt đầu" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Thời gian bắt đầu</SelectLabel>
+                    {times.map((time) => (
+                      <SelectItem value={time.time} key={time.id}>
+                        {time.time}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Select name={`tillTime-${item.id}`} defaultValue={item.tillTime}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Thời gian kết thúc" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Thời gian kết thúc</SelectLabel>
+                    {times.map((time) => (
+                      <SelectItem value={time.time} key={time.id}>
+                        {time.time}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
-
-            <Select defaultValue={item.fromTime}>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Thời gian bắt đầu" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  {times.map((time) => (
-                    <SelectItem value={time.time} key={time.id}>
-                      {time.time}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-
-            <Select defaultValue={item.tillTime}>
-              <SelectTrigger className="w-full">
-                <SelectValue placeholder="Thời gian kết thúc" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectGroup>
-                  {times.map((time) => (
-                    <SelectItem value={time.time} key={time.id}>
-                      {time.time}
-                    </SelectItem>
-                  ))}
-                </SelectGroup>
-              </SelectContent>
-            </Select>
-          </div>
-        ))}
-      </CardContent>
-      <CardFooter>
-        <SubmitButton text="Lưu thay đổi" />
-      </CardFooter>
+          ))}
+        </CardContent>
+        <CardFooter>
+          <SubmitButton text="Lưu thay đổi" />
+        </CardFooter>
+      </form>
     </Card>
   );
 }

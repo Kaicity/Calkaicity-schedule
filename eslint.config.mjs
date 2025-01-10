f40fe05e16b375a0b531314334e44f2078a85b8c
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-01-10 11:17:27 +0700

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [...compat.extends('next/core-web-vitals')];
 
+// const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+
 export default eslintConfig;
